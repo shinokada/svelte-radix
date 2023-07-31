8dd834e79036c34a6fb9e964fbe5c7195baa8343
Shinichi Okada <okada.shin@gmail.com>
2023-07-31 18:13:56 +0200

    fix: component doc update

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -35,7 +35,7 @@ viewBox="0 0 {displayIcon.box} {displayIcon.box}"
 
 <!--
 @component
-[Go to Document](https://shinokada.github.io/svelte-radix/)
+[Go to Document](https://svelte-radix.vercel.app/)
 ## Props
 @prop name;
 @prop width = "15";
