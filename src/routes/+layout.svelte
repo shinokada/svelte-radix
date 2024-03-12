c2ce11a55b1e12c95c1cd8e6fc8d9e4525e2a848
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-12 17:00:54 +0100

    fix: change to codewithshin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -79,7 +79,7 @@
         <NavLi class="lg:px-2 lg:mb-0" href="https://github.com/shinokada/svelte-radix"
           >GitHub</NavLi
         >
-        <NavLi href="https://svelte-svg-icons.vercel.app/">Icon sets</NavLi>
+        <NavLi href="https://svelte-svg-icons.codewithshin.com/">Icon sets</NavLi>
       </NavUl>
       <div class="flex items-center ml-auto">
         <DarkMode class="inline-block dark:hover:text-white hover:text-gray-900" />
@@ -94,8 +94,8 @@
   <Footer footerType="logo" class="dark:bg-slate-950">
     <div class="sm:flex sm:items-center sm:justify-between">
       <FooterBrand
-        href="https://svelte-radix.vercel.app/"
-        name="Svelte Radix"
+        href="https://codewithshin.com/"
+        name="codewithshin.com"
         classSpan="text-primary-700 dark:text-primary-500"
       />
       <FooterLinkGroup
@@ -106,7 +106,7 @@
         <FooterLink class="lg:px-2 lg:mb-0" href="https://github.com/shinokada/svelte-radix"
           >GitHub</FooterLink
         >
-        <FooterLink href="https://svelte-svg-icons.vercel.app/">Icon sets</FooterLink>
+        <FooterLink href="https://svelte-svg-icons.codewithshin.com/">Icon sets</FooterLink>
       </FooterLinkGroup>
     </div>
   </Footer>
